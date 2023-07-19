bbffa89e55c9d1983716becbdabb98076f411631
TonyHdez-Pi <tonyhdezpi@gmail.com>
2023-07-19 15:12:53 -0600

    Added Link tag
    -added Link to improve the load time.

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,16 +1,28 @@
-import Image from "next/image"
-import navbarcss from './navbar.module.css'
+import Image from "next/image";
+import Link from "next/link";
+import navbarcss from "./navbar.module.css";
 export default function Navbar() {
-  return (
-    <div className={navbarcss.navbar}>
-    <Image width={200} height={100} src="/webooks-logo.webp" alt="" />
-    
-     <ul className={navbarcss.navbar__options}>
-       <li><a className={navbarcss.a} href="/">Inicio</a></li>
-       <li><a href="/books">Libros</a></li>
-       <li> <a href="#">Quienes somos</a> </li>
-       <li><a href="#">Invitame un cafe</a></li>
-     </ul>
-    </div>
-  )
+	return (
+		<div className={navbarcss.navbar}>
+			<Image width={200} height={100} src="/webooks-logo.webp" alt="" />
+
+			<ul className={navbarcss.navbar__options}>
+				<li>
+					<Link className={navbarcss.a} href="/">
+						Inicio
+					</Link>
+				</li>
+				<li>
+					<Link href="/books">Libros</Link>
+				</li>
+				<li>
+					{" "}
+					<Link href="#">Quienes somos</Link>{" "}
+				</li>
+				<li>
+					<Link href="#">Invitame un cafe</Link>
+				</li>
+			</ul>
+		</div>
+	);
 }
